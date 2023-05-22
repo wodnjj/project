df87364c9aad985650ebfbefdef830635cb92d27
wodnjj park <cgk7522@naver.com>
2023-05-22 07:04:51 +0000

    프로젝트 하는 중

---
+++ ball_sd.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://sports.news.naver.com/wfootball/schedule/index?category=epl"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rank = 1
+    for tr in soup.select("#frm > div > table > tbody > tr"):
+        title = tr.select_one(".ellipsis.rank01 > span > a").text
+        artist = tr.select_one(".ellipsis.rank02 > a").text
+        print(rank, title, artist)
+        rank += 1
+else:
+        print(f"HTTP 요청 실패 코드: {response.status_code}")
---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+base_url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjA5&qvt=0&query="
+keyword= input("검색어를 입력하세요 : ")
+
+search_url = base_url + keyword
+
+r = requests.get(search_url)
+
+soup = BeautifulSoup(r.text. "html.parser")
